e81f79d97e9c7d57c4d93409c997a2439a78ba33
zztkm <zztkm@tsurutatakumi.info>
2024-06-02 23:03:13 +0900

    fix: cat command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// #86442 <https://github.com/rust-lang/rust/issues/86442>
+#![feature(io_error_more)]
+
 use clap::{Parser, Subcommand};
 use std::io::{Read, Write};
 
@@ -24,14 +27,21 @@ fn main() {
 
                 // files を入力順に stdout に出力する
                 // 最後のファイルは改行をしない
-                let mut iter = files.iter().peekable();
-                while let Some(file) = iter.next() {
-                    let content = std::fs::read_to_string(file).unwrap();
-                    if iter.peek().is_none() {
-                        write!(&mut stdout, "{}", content).unwrap();
-                    } else {
-                        writeln!(&mut stdout, "{}", content).unwrap();
-                    }
+                for file in files {
+                    let content = match get_file_data(file) {
+                        Ok(content) => content,
+                        Err(e) => match e.kind() {
+                            std::io::ErrorKind::NotFound => {
+                                format!("ztb cat: {}: No such file or directory\n", file)
+                            }
+                            std::io::ErrorKind::IsADirectory => {
+                                format!("ztb cat: {}: Is a directory\n", file)
+                            }
+                            _ => format!("ztb cat: {}: {}", file, e),
+                        },
+                    };
+
+                    write!(&mut stdout, "{}", content).unwrap();
                 }
             } else {
                 // files の指定がない場合は標準入力から読み取る
@@ -47,4 +57,9 @@ fn main() {
             }
         }
     }
+}
+
+/// 与えたら path のファイル内容を返します。
+fn get_file_data(path: &str) -> std::io::Result<String> {
+    std::fs::read_to_string(path)
 }
